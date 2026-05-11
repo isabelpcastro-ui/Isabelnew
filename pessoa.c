91841b20add556d83737df6279c0a86c85142ff0
isabelpcastro-ui <isabel.p.castro@edu.ufes.br>
2026-05-11 12:47:35 -0300

    Add files via upload

---
+++ pessoa.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pessoa.h"
+
+/* tPessoa **pessoas = (tPessoa **)malloc(sizeof(tPessoa));  Pois tenho um ponteiro de ponteiros(no caso, um ponteiro tPessoa de ponteiros pessoa)*/
+tPessoa* criaPessoa()
+{
+
+}
+void liberaPessoa(tPessoa *p)
+{
+    free(p);
+}
+void setNomePessoa(tPessoa *p)
+{
+}
+void setDataNascimento(tPessoa *p)
+{
+
+}
+void setCPF(tPessoa *p)
+{
+
+}
+void printaPessoa(tPessoa *p)
+{
+
+}
+
+char* getNome(tPessoa *p)
+{
+
+}
+int getDiaNascimento(tPessoa *p)
+{
+
+}
+int getMesNascimento(tPessoa *p)
+{
+    
+}
+int getAnoNascimento(tPessoa *p)
+{
+
+}
+char* getCPF(tPessoa *p)
+{
+
+}
